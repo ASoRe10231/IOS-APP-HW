04b4da457c1057409d407bb0f8f6c9411c7a7d2d
RTC08 <rtc08@rtc08de-imac.local>
2017-10-13 13:48:31 +0800

    Initial Commit

---
+++ 555/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  555
+//
+//  Created by RTC08 on 2017/10/13.
+//  Copyright © 2017年 RTC08. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
